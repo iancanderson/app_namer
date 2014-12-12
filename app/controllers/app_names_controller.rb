e1bc64a26ce0a463f23ab31ee1282cee6ef7dc11
Ian C. Anderson <ian@redheadedlabs.com>
2014-12-11 20:29:24 -0500

    minimum_word_count of 5 for mottos

--- app/controllers/app_names_controller.rb
+++ app/controllers/app_names_controller.rb
@@ -29,6 +29,7 @@ class AppNamesController < ApplicationController
 
     pun = GirlsJustWantToHavePuns.pun(
       params[:direct_object],
+      minimum_word_count: 5,
       rhymes: spelling.rhymes
     )
 
